c11d11dd082c78dc1b062a582f82b97c08d856ef
TungusMur <vova.myraw@gmail.com>
2021-12-19 01:55:20 +0400

    v3.0

--- VolkovLab3/VolkovLab3/VolkovLab3.cpp
+++ VolkovLab3/VolkovLab3/VolkovLab3.cpp
@@ -1,29 +1,33 @@
-﻿#include <windows.h>
+﻿#include <iostream>
+#include <windows.h>
 #include <conio.h>
-#include <iostream>
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #include <fstream>
-#include <vector>
 
 using namespace std;
 
 struct emp
 {
-	int num; // номер зачетки
-	char name[10]; // имя студента
-	double grade; // средний бал
+	int num = 0; // номер зачетки
+	char name[10] = ""; // имя студента
+	double grade = 0; // средний бал
 };
 
-void showStudents() {
-	emp iCredits;
-	cout << "---Вывод студентов---\n";
-	ifstream iFileOld(".\\file.txt", ios::binary);
-	while (iFileOld.peek() != EOF) {
-		iFileOld.read((char*)&iCredits, sizeof(iCredits));
-		cout << "Номер зачетки: " << iCredits.num << "|Имя студента: " << iCredits.name << "|Средний бал: " << iCredits.grade << "\n\n";
+void showStudents(FILE* bfile, emp wValue) {
+	int pos = 0;
+
+	while (!feof(bfile)) {
+		fseek(bfile, pos * sizeof(emp), SEEK_SET);
+		fread(&wValue, sizeof(emp), 1, bfile);
+
+		if (!feof(bfile)) {
+			cout << "Номер зачетки: " << wValue.num << "|Имя студента: " << wValue.name << "|Средний бал: " << wValue.grade << "\n\n";
+		}
+
+		pos++;
 	}
-	iFileOld.close();
 }
 
 int main()
@@ -31,16 +35,116 @@ int main()
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 
+	FILE* bfile = NULL;
+	int pos = 0,
+		check;
+	emp wValue;
+	char num[10]; // номер зачетки
+	char name[10]; // имя студентаx
+	char newGrade[10]; // НОВЫЙ средний бал
+
+	fopen_s(&bfile, ".//file.txt", "r+b");
+
+	if (bfile == NULL) {
+		fopen_s(&bfile, ".//file.txt", "wb");
+		fclose(bfile);
+		fopen_s(&bfile, ".//file.txt", "r+b");
+	}
+
+	while (true) {
+
+		showStudents(bfile, wValue);
+
+		cout << "Добавить студента ?\n	1 - да\n	2 - нет\n";
+		cin >> check;
+
+		if (check == 1) {
+			cout << "---Ввод студентов---\n";
+
+			cout << "Номер зачетки:	";
+			cin >> wValue.num;
+
+			cin.get();
+			cout << "Имя студента:	";
+			gets_s(wValue.name);
+
+			cout << "Средний бал:	";
+			cin >> wValue.grade;
+
+			cout << "\n";
+
+			fwrite(&wValue, sizeof(emp), 1, bfile);
+		}
+		else {
+			break;
+		}
+	}
+
+	fclose(bfile);
+
+	cout << "---Обновление ср.оценки студента---\n";
+
+	cout << "Номер зачетки:	";
+	cin.get();
+	gets_s(num);
+
+	cout << "Имя студента:	";
+	gets_s(name);
+
+	cout << "Средний бал:	";
+	gets_s(newGrade);
+
+	cout << "\n";
+
+	char lpszCommandLine[256];
+	sprintf_s(lpszCommandLine, "E:\\Microsoft Visual Studio\\Project\\VolkovLab3Child\\Debug\\VolkovLab3Child.exe %s %s %s", num, name, newGrade);
+
+	STARTUPINFO si;
+	PROCESS_INFORMATION piCom;
+	ZeroMemory(&si, sizeof(STARTUPINFO));
+	si.cb = sizeof(STARTUPINFO);
+
+	// Запускает дочерний процесс Child, которому как параметр передается командная строка, сформированная в пункте 4.
+	if (!CreateProcess(NULL, (LPSTR)lpszCommandLine, NULL, NULL, FALSE,
+		CREATE_NEW_CONSOLE, NULL, NULL, &si, &piCom))
+
+	{
+		_cputs("The new process is not created.\n");
+		_cputs("Check a name of the process.\n");
+		return 0;
+	}
+
+
+	// Ждет завершения работы процесса Child.	
+	WaitForSingleObject(piCom.hProcess, INFINITE);
+	// закрываем дескрипторы этого процесса в текущем процессе
+	CloseHandle(piCom.hThread);
+	CloseHandle(piCom.hProcess);
+
+	fopen_s(&bfile, ".//file.txt", "r+b");
+	showStudents(bfile, wValue);
+	fclose(bfile);
+
+
+
+
+
+
+
+
+
+
+
+	/*
+
 	const int N = 2;
 	emp oCredits;
-	vector <emp> Credits;
 	emp iCredits;
 	emp iCreditsNew;
 	emp iCreditsOld;
 	char num[10]; // номер зачетки
 	char name[10]; // имя студента
 	char newGrade[10]; // средний бал
-	int check;
 
 	showStudents();
 
@@ -103,7 +207,7 @@ int main()
 	}
 
 
-	// 6. Ждет завершения работы процесса Child.
+	// 6. Ждет завершения работы процесса Child.	
 	WaitForSingleObject(piCom.hProcess, INFINITE);
 	// закрываем дескрипторы этого процесса в текущем процессе
 	CloseHandle(piCom.hThread);
@@ -113,6 +217,6 @@ int main()
 	// 7. Выводит откорректированный файл на консоль.
 	showStudents();
 
-	// 8. Завершает свою работу.
+	// 8. Завершает свою работу.*/
 	return 0;
 }
--- VolkovLab3Child/VolkovLab3Child/VolkovLab3Child.cpp
+++ VolkovLab3Child/VolkovLab3Child/VolkovLab3Child.cpp
@@ -1,7 +1,10 @@
 ﻿#include <iostream>
 #include <fstream>
+#include <limits>
+#include <stdio.h>
 #include <conio.h>
 #include <vector>
+#include <stdlib.h>
 #include <Windows.h>
 
 using namespace std;
@@ -17,31 +20,78 @@ int main(int argc, char* argv[])
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 
-	cout << "Номер зачетки: " << argv[1] << "|Имя студента: " << argv[2] << "|Средний бал: " << argv[3] << "\n\n";
+	emp wValue;
+	FILE* bfile = NULL;
+	int pos = 0,
+		num;
+	double newGrade = 1.0;
+	char cNum[255];
 
-	const int N = 2;
+	cout << "*****";
+	cout << "Номер зачетки: " << argv[1] << "|Имя студента: " << argv[2] << "|Средний бал: " << argv[3] << "\n\n";
+	cout << "*****\n";
+	
+	fopen_s(&bfile, "E:/Microsoft Visual Studio/Project/VolkovLab3/VolkovLab3/file.txt", "r+b");
+
+	while (!feof(bfile)) {
+		fseek(bfile, pos * sizeof(emp), SEEK_SET);
+		fread(&num, sizeof(int), 1, bfile);
+		
+		sprintf_s(cNum, "%d", num);
+
+		if(!feof(bfile) && (strcmp(cNum, argv[1]) == 0)) {
+			fseek(bfile, sizeof(char[10]), SEEK_CUR);
+			/**
+			newGrade = atof(argv[3]);
+			cout << newGrade;
+			*/
+			fread(&newGrade, sizeof(double), 3, bfile);
+			cout << newGrade;
+			break;
+		}
+
+		pos++;
+	}
+
+	fclose(bfile);
+
+	_getch();
+
+
+
+
+
+
+
+
+	/*const int N = 2;
+	int p = 0;
 	emp iCreditsNew;
-	vector <emp> CreditsNew;
 	char num[255];
+	char t;
 
 	ifstream iFile("E:\\Microsoft Visual Studio\\Project\\VolkovLab3\\VolkovLab3\\file.txt", ios::binary);
 	while (iFile.peek() != EOF) {
+		p = iFile.tellg();
 		iFile.read((char*)&iCreditsNew, sizeof(iCreditsNew));
 		sprintf_s(num, "%d", iCreditsNew.num);
-
-		if ((strcmp(iCreditsNew.name, argv[2]) == 0) && (strcmp(num, argv[1]) == 0)) {
+		if (strcmp(num, argv[1]) == 0) {
 			sscanf_s(argv[3], "%lf", &iCreditsNew.grade);
+			//iFile.write((char*)&iCreditsNew, sizeof(iCreditsNew));
+			break;
 		}
-		CreditsNew.push_back(iCreditsNew);
 	}
 	iFile.close();
 
-
-	ofstream oFile(".\\file.txt", ios::binary);
-	for (emp item : CreditsNew) {
-		oFile.write((char*)&item, sizeof(item));
+	fstream oFile("E:\\Microsoft Visual Studio\\Project\\VolkovLab3\\VolkovLab3\\file.txt", ios::in | ios::out | ios::binary);
+	for (int i = 0; i < 24; i++) {
+		oFile.seekp(p + i, ios::beg);
+		oFile.put('\0');
 	}
-	oFile.close();
+	oFile.seekp(p, ios::beg);
+	cout << "\n" << oFile.tellg() << "\n";
+	oFile.write((char*)&iCreditsNew, sizeof(iCreditsNew));
+	oFile.close();*/
 
 	system("PAUSE");
 }
